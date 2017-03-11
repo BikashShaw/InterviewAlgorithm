b16fbbac7b1f0a642c49846897ddbe716bfbc72a
Bikash Shaw <bikashshaw@gmail.com>
2017-03-11 12:57:45 -0500

    Queue impl

--- DataStructure/src/linear/datastructure/Queue.java
+++ DataStructure/src/linear/datastructure/Queue.java
@@ -7,5 +7,33 @@ package linear.datastructure;
 public class Queue {
     Node first, last;
 
-//    public void
+    public void enQueue(int element) {
+        if(first == null) {
+            last = new Node(element);
+            first = last;
+        } else {
+            last.next = new Node(element);;
+            last = last.next;
+        }
+    }
+
+    public Node deQueue() {
+        if(first != null) {
+            Node item = first;
+            first = first.next;
+            return item;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enQueue(5);
+        queue.enQueue(7);
+        queue.enQueue(9);
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+    }
 }

e505f99cb4cce29c743a377736c93c5d0fdaa090
Bikash Shaw <bikashshaw@gmail.com>
2017-03-16 19:07:49 -0400

    Sort by Frequency at Time Complexity: O(n)

---
+++ ArrayAndString/src/arrayandstring/SortByFrequency.java
@@ -0,0 +1,42 @@
+package arrayandstring;
+
+import java.util.*;
+
+/**
+ * Sort by Frequency at Time Complexity: O(n)
+ * Created by Bikash on 3/16/2017.
+ */
+public class SortByFrequency {
+
+    public static String sort(String givenString) {
+        StringBuilder sortedStrint = new StringBuilder();
+        final Hashtable<Character, Integer> characterIntegerHashtable = new Hashtable<>();
+        List<Character> sortedList = new ArrayList<>();
+
+        for (int i = 0; i < givenString.length() ; i++) {
+            char ch = givenString.charAt(i);
+            sortedList.add(ch);
+            if(characterIntegerHashtable.containsKey(ch)) {
+                characterIntegerHashtable.put(ch, characterIntegerHashtable.get(ch) + 1);
+            } else {
+                characterIntegerHashtable.put(ch, 1);
+            }
+        }
+
+        Collections.sort(sortedList, new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return characterIntegerHashtable.get(o2) - characterIntegerHashtable.get(o1) ;
+            }
+        });
+
+        for (Character character : sortedList) {
+            sortedStrint.append(character);
+        }
+        return sortedStrint.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sort("DCCCCCAABBB"));
+    }
+}

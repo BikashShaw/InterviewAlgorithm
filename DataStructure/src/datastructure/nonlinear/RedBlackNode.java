d4fe818ebc73ba136c8fac7c0631704a58bf2505
uc0010 <BSHAW-EXTERNAL@scor.com>
2017-03-30 12:27:55 -0400

    Red Black initial commit

---
+++ DataStructure/src/datastructure/nonlinear/RedBlackNode.java
@@ -0,0 +1,16 @@
+package datastructure.nonlinear;
+
+/**
+ * Created by Bikash on 3/30/2017.
+ *
+ */
+public class RedBlackNode {
+
+    int data;
+    RedBlackNode leftChild, rightChild;
+
+    public RedBlackNode(int data) {
+        this.data = data;
+
+    }
+}
---
+++ DataStructure/src/datastructure/nonlinear/RedBlackTree.java
@@ -0,0 +1,22 @@
+package datastructure.nonlinear;
+
+/**
+ * Created by Bikash on 3/30/2017.
+ *  <ol>
+ *     <li>
+ *         Every node is either red or black.
+ *     </li>
+ *     <li>
+ *         Every leaf (NIL) is black.
+ *     </li>
+ *     <li>
+ *         If a node is red, then both its children are black.
+ *     </li>
+ *     <li>
+ *         Every simple path from a node to a descendant leaf contains the same number of black nodes.
+ *     </li>
+ * </ol>
+ */
+public class RedBlackTree {
+
+}

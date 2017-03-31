8c06886584481726feb7bba69f4922415cd03611
uc0010 <BSHAW-EXTERNAL@scor.com>
2017-03-31 15:41:48 -0400

    Binary search first occurrence, last occurrence, and count total occurrence.

--- SearchAndSort/src/search/BinarySearch.java
+++ SearchAndSort/src/search/BinarySearch.java
@@ -3,12 +3,12 @@ package search;
 import java.util.Arrays;
 
 /**
- * Recursive Binary Search
- * Time Complexity: O(log n)
+ * Various binary search operations
  * Created by Bikash on 3/26/2017.
  */
 public class BinarySearch {
-
+//    Time Complexity: O(log n)
+//    Recursive Binary Search
     public int search(int a[], int low, int high, int target) {
 
         if(low > high) {
@@ -25,6 +25,56 @@ public class BinarySearch {
         }
     }
 
+    //Time Complexity O(logn)
+    public int searchFirstOccurrence(int a[], int target) {
+        int low = 0;
+        int high = a.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+
+            if(target == a[mid]) {
+                result = mid;
+                high = mid - 1;
+            }
+            else if(target > a[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+
+    }
+
+    //Time Complexity O(logn)
+    public int searchLastOccurrence(int a[], int target) {
+        int low = 0;
+        int high = a.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if(target == a[mid]) {
+                result = mid;
+                low = mid + 1;
+            } else if(target > a[mid]) {
+                low = mid +1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    //Time Complexity O(logn)
+    public int countOccurrence(int a[], int target) {
+        return searchLastOccurrence(a, target) - searchFirstOccurrence(a,target) + 1;
+    }
+
+
     public static void main(String[] args) {
         int a[] = {1,2,3,4,5,6,9,10,11,12};
         BinarySearch binarySearch = new BinarySearch();
@@ -36,5 +86,17 @@ public class BinarySearch {
         System.out.println("Found 14: " + binarySearch.search(a, 0, a.length-1, 14));
         System.out.println("Found 0: " + binarySearch.search(a, 0, a.length-1, 0));
         System.out.println("Found 1: " + binarySearch.search(a, 0, a.length-1, 1));
+
+        int b[] = {2,2,2,2,2,2,4,4,4,4,10,10,10,18,18,20,20,20,20,20};
+
+        System.out.println("First occurrence of 10: " + binarySearch.searchFirstOccurrence(b, 10));
+        System.out.println("Any occurrence of 10: " + binarySearch.search(b, 0, b.length-1, 10));
+        System.out.println("Last occurrence of 10: " + binarySearch.searchLastOccurrence(b, 10));
+        System.out.println("Count total occurrences of 10: " + binarySearch.countOccurrence(b, 10));
+
+        System.out.println("First occurrence of 2: " + binarySearch.searchFirstOccurrence(b, 2));
+        System.out.println("Any occurrence of 2: " + binarySearch.search(b, 0, b.length-1, 2));
+        System.out.println("Last occurrence of 2: " + binarySearch.searchLastOccurrence(b, 2));
+        System.out.println("Count total occurrences of 2: " + binarySearch.countOccurrence(b, 2));
     }
 }

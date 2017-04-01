7a6c905e696fe4ae2a35674c07aca4d795a4b457
Bikash Shaw <bikashshaw@gmail.com>
2017-03-31 22:33:41 -0400

    Minor update

--- ArrayAndString/src/arrayandstring/dynamicprograming/LongestCommonSubstring.java
+++ ArrayAndString/src/arrayandstring/dynamicprograming/LongestCommonSubstring.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * Longest Common Substring
  * Time Complexity: O(n * m)
- * Created by uc0010 on 3/23/2017.
+ * Created by Bikash on 3/23/2017.
  */
 public class LongestCommonSubstring {
 
--- DataStructure/src/datastructure/nonlinear/RedBlackColor.java
+++ DataStructure/src/datastructure/nonlinear/RedBlackColor.java
@@ -1,7 +1,7 @@
 package datastructure.nonlinear;
 
 /**
- * Created by uc0010 on 3/30/2017.
+ * Created by Bikash on 3/30/2017.
  */
 public enum RedBlackColor {
     RED, BLACK;

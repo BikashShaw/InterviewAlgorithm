1543b80c8fdb704c856dae89013a4fabf11e0a3a
Bikash Shaw <bikashshaw@gmail.com>
2017-04-04 16:44:31 -0400

    Lonely integer problem - hacker rank

---
+++ ArrayAndString/src/arrayandstring/LonelyInteger.java
@@ -0,0 +1,20 @@
+package arrayandstring;
+
+/**
+ * Find one single integer among multiple even number of integers
+ * Created by Bikash on 4/4/2017.
+ */
+public class LonelyInteger {
+
+    int findLonley(int a[]) {
+        int result = 0;
+        for (int i : a) {
+            result ^= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LonelyInteger().findLonley(new int[] {9, 1, 2, 3, 2, 9, 1, 7, 7}));
+    }
+}

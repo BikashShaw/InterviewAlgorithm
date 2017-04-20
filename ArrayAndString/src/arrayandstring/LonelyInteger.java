d76f331b06e4ae19ff081f10feba4d666a646e9d
uc0010 <BSHAW-EXTERNAL@scor.com>
2017-04-20 17:01:29 -0400

    Improved 1 line solution

--- ArrayAndString/src/arrayandstring/LonelyInteger.java
+++ ArrayAndString/src/arrayandstring/LonelyInteger.java
@@ -1,20 +1,18 @@
 package arrayandstring;
 
+import java.util.Arrays;
+
 /**
  * Find one single integer among multiple even number of integers
  * Created by Bikash on 4/4/2017.
  */
 public class LonelyInteger {
 
-    int findLonely(int a[]) {
-        int result = 0;
-        for (int i : a) {
-            result ^= i;
-        }
-        return result;
+    int findLonely(Integer a[]) {
+        return Arrays.stream(a).reduce((left, right) -> left ^ right).get();
     }
 
     public static void main(String[] args) {
-        System.out.println(new LonelyInteger().findLonely(new int[] {9, 1, 2, 3, 2, 9, 1, 7, 7}));
+        System.out.println(new LonelyInteger().findLonely(new Integer[]{10, 9, 1, 2, 3, 3, 11, 2, 9, 1, 7, 7, 10}));
     }
 }

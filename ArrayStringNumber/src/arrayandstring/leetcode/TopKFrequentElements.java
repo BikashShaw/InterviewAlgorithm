97ff9d93c2d1551a82fd120645a1f71a251f75d6
Bikash Shaw <bikashshaw@gmail.com>
2017-04-25 23:57:19 -0400

    Given a non-empty array of integers, return the k most frequent elements.
    For example,
    Given [1,1,1,2,2,3] and k = 2, return [1,2].
    
    Note:
    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
    Your algorithm's time complexity must be better than O(n log n), where n is the array's size.

---
+++ ArrayStringNumber/src/arrayandstring/leetcode/TopKFrequentElements.java
@@ -0,0 +1,39 @@
+package arrayandstring.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Given a non-empty array of integers, return the k most frequent elements.
+
+ For example,
+ Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+ Note:
+ You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * Created by Bikash on 4/25/2017.
+ */
+public class TopKFrequentElements {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Long> result = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        result.entrySet().stream().sorted(Map.Entry.<Integer, Long>comparingByValue().reversed()).forEachOrdered(e -> addToList(list,e.getKey(),k));
+
+        return list;
+    }
+
+    public void addToList(List<Integer> list, Integer value, int k) {
+        if(list.size()<k) {
+            list.add(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        new TopKFrequentElements().topKFrequent(new int[] {1,1,1,2,2,3},2).forEach(System.out::println);
+    }
+}

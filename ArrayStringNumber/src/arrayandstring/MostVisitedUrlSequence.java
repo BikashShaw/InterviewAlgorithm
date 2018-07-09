093cd4424627a98ade9ebefd1fcfe5405e63e772
uc0010 <BSHAW-EXTERNAL@scor.com>
2018-07-09 10:28:32 -0400

    Most visited URL sequence

--- ArrayStringNumber/src/arrayandstring/MostVisitedUrlSequence.java
+++ ArrayStringNumber/src/arrayandstring/MostVisitedUrlSequence.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class MostVisitedUrlSequence {
     private static Map<String, List<String>> urlUserMap;
@@ -12,11 +11,11 @@ public class MostVisitedUrlSequence {
 
     static {
         urlUserMap = new HashMap<>();
-        urlUserMap.put("u1", Arrays.stream(new String[]{"a", "b", "c", "d", "e"}).collect(Collectors.toList()));
-        urlUserMap.put("u2", Arrays.stream(new String[]{"c", "d", "e", "f", "g", "a"}).collect(Collectors.toList()));
-        urlUserMap.put("u3", Arrays.stream(new String[]{"a", "b", "c", "d", "e", "f"}).collect(Collectors.toList()));
-        urlUserMap.put("u4", Arrays.stream(new String[]{"d", "e", "f"}).collect(Collectors.toList()));
-        urlUserMap.put("u5", Arrays.stream(new String[]{"c", "d", "e", "f"}).collect(Collectors.toList()));
+        urlUserMap.put("u1", Arrays.asList("a", "b", "c", "d", "e"));
+        urlUserMap.put("u2", Arrays.asList("c", "d", "e", "f", "g", "a"));
+        urlUserMap.put("u3", Arrays.asList("a", "b", "c", "d", "e", "f"));
+        urlUserMap.put("u4", Arrays.asList("d", "e", "f"));
+        urlUserMap.put("u5", Arrays.asList("c", "d", "e", "f"));
     }
 
     public static void main(String[] args) {

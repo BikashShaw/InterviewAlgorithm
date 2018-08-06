c92f5f4dd989d43905ca243f48ee03473b5b0b0c
bikash-at-640172929504 <pZREc9L8W9MxlXf3PI5Dyct/MN+RdQ4lKjymK9f6i+g=>
2018-08-05 20:06:10 -0400

    TODO: code fix

--- ArrayStringNumber/src/arrayandstring/Permutation.java
+++ ArrayStringNumber/src/arrayandstring/Permutation.java
@@ -39,7 +39,48 @@ public class Permutation {
         return str.substring(0, location) + positionChar + str.substring(location, str.length());
     }
 
+
+    /**
+     * permutation function
+     * @param str string to calculate permutation for
+     * @param l starting index
+     * @param r end index
+     */
+    private void permute(String str, int l, int r)
+    {
+        if (l == r)
+            System.out.println(str);
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+
+    /**
+     * Swap Characters at position
+     * @param a string value
+     * @param i position 1
+     * @param j position 2
+     * @return swapped string
+     */
+    public String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+
     public static void main(String[] args) {
         System.out.println( new Permutation().permutations("ABC"));
+
+        new Permutation().permute("ABC", 0 , 2);
     }
 }

8de32bff199f57a9b20b6de3ed85d1f194462e28
bikash-at-640172929504 <pZREc9L8W9MxlXf3PI5Dyct/MN+RdQ4lKjymK9f6i+g=>
2018-08-11 10:48:39 -0400

    DRY code

--- MultiThreading/src/basic/JoinDemo.java
+++ MultiThreading/src/basic/JoinDemo.java
@@ -2,28 +2,23 @@ package basic;
 
 public class JoinDemo {
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(() -> {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("T1");
-        }, "T1");
+        Thread t1 = new Thread(() -> createRunnable(10), "T1");
 
-        Thread t2 = new Thread(() -> {
-            try {
-                Thread.sleep(30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("T2");
-        }, "T2");
+        Thread t2 = new Thread(() -> createRunnable(30), "T2");
 
         t1.start();
         t2.start();
 
         t1.join();
         System.out.println("Main Finished!");
+    }
+
+    private static void createRunnable(int sleepTime) {
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName());
     }
 }

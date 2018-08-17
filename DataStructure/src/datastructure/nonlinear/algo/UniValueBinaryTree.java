6534f4a19ffc03a951e66132dd12291bc4c5d8e2
uc0010 <BSHAW-EXTERNAL@scor.com>
2018-08-17 15:06:22 -0400

    UniValue binary tree

--- DataStructure/src/datastructure/nonlinear/algo/UniValueBinaryTree.java
+++ DataStructure/src/datastructure/nonlinear/algo/UniValueBinaryTree.java
@@ -3,6 +3,9 @@ package datastructure.nonlinear.algo;
 import datastructure.nonlinear.BinaryTree;
 import datastructure.nonlinear.BinaryTreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UniValueBinaryTree {
 
     public static void main(String[] args) {
@@ -21,15 +24,42 @@ public class UniValueBinaryTree {
 
         BinaryTree uniValBinarySearchTree = new BinaryTree();
         uniValBinarySearchTree.binaryBalancedTreeInsert(5);
-        uniValBinarySearchTree.binaryBalancedTreeInsert(5);
-        uniValBinarySearchTree.binaryBalancedTreeInsert(5);
-        uniValBinarySearchTree.binaryBalancedTreeInsert(5);
+        uniValBinarySearchTree.binaryBalancedTreeInsert(3);
+        uniValBinarySearchTree.binaryBalancedTreeInsert(3);
+        uniValBinarySearchTree.binaryBalancedTreeInsert(3);
         uniValBinarySearchTree.binaryBalancedTreeInsert(5);
         uniValBinarySearchTree.binaryBalancedTreeInsert(5);
         uniValBinarySearchTree.binaryBalancedTreeInsert(5);
 
         System.out.println(isUniValueBinaryTree(uniValBinarySearchTree.root));
+        System.out.println(uniValueBinaryTreeCount(uniValBinarySearchTree.root));
 
+    }
+
+    public static int uniValueBinaryTreeCount(BinaryTreeNode root) {
+        int counter = 0;
+        if (root == null) {
+            counter = 0;
+        } else if (root.leftChild == null && root.rightChild == null) {
+            counter = 1;
+        } else {
+            Queue<BinaryTreeNode> nodeQueue = new LinkedList<>();
+            nodeQueue.add(root);
+
+            while (!nodeQueue.isEmpty()) {
+                BinaryTreeNode node = nodeQueue.poll();
+                if (isUniValueBinaryTree(node)) {
+                    counter++;
+                }
+                if (node.leftChild != null) {
+                    nodeQueue.add(node.leftChild);
+                }
+                if (node.rightChild != null) {
+                    nodeQueue.add(node.rightChild);
+                }
+            }
+        }
+        return counter;
     }
 
     public static boolean isUniValueBinaryTree(BinaryTreeNode root) {

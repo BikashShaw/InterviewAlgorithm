4bc13f91a10ec9b45e42c232701280eec14e5681
uc0010 <BSHAW-EXTERNAL@scor.com>
2018-08-20 16:33:45 -0400

    Palindromic Substrings

--- ArrayStringNumber/src/string/palindromicsubstrings/PalindromicSubstringsSolution1.java
+++ ArrayStringNumber/src/string/palindromicsubstrings/PalindromicSubstringsSolution1.java
@@ -1,4 +1,4 @@
-package string;
+package string.palindromicsubstrings;
 
 import java.util.ArrayList;
 import java.util.List;
--- ArrayStringNumber/src/string/palindromicsubstrings/PalindromicSubstringsSolution2.java
+++ ArrayStringNumber/src/string/palindromicsubstrings/PalindromicSubstringsSolution2.java
@@ -1,4 +1,4 @@
-package string;
+package string.palindromicsubstrings;
 
 public class PalindromicSubstringsSolution2 {
 

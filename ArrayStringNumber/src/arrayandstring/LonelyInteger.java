f788bb699cb47f88402528740f64a0f67845ea53
bikash-at-640172929504 <pZREc9L8W9MxlXf3PI5Dyct/MN+RdQ4lKjymK9f6i+g=>
2018-08-20 22:59:06 -0400

    reformating

--- ArrayStringNumber/src/arrayandstring/LonelyInteger.java
+++ ArrayStringNumber/src/arrayandstring/LonelyInteger.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class LonelyInteger {
 
     int findLonely(int a[]) {
-        return Arrays.stream(a).reduce((left, right) -> left ^ right).getAsInt();
+        return Arrays.stream(a).reduce((left, right) -> left ^ right).orElse(-1);
     }
 
     /**
@@ -18,31 +18,35 @@ public class LonelyInteger {
      * Find this single element that appears only once.
      * <br />
      * <strong>Note:</strong> Your solution should run in O(log n) time and O(1) space.
+     *
      * @param nums
      * @return
      */
     public int singleNonDuplicate(int[] nums) {
-        int low = 0, high = nums.length /2 ;
+
+        System.out.println(Arrays.toString(nums));
+
+        int low = 0, high = nums.length / 2;
 
         while (low < high) {
-            int mid = (low + high) /2;
+            int mid = (low + high) / 2;
             System.out.println("Low: " + low + " Mid: " + mid + " High: " + high);
-            System.out.println("nums[2*mid]: " + nums[2*mid]);
-            System.out.println("nums[2*mid+1]: " + nums[2*mid+1]);
-            if(nums[2*mid] != nums[2*mid+1]) {
-                high=mid;
+            System.out.println("nums[2*mid]: " + nums[2 * mid]);
+            System.out.println("nums[2*mid+1]: " + nums[2 * mid + 1]);
+            if (nums[2 * mid] != nums[2 * mid + 1]) {
+                high = mid;
                 System.out.println("High : " + high);
             } else {
-                low = mid +1;
+                low = mid + 1;
                 System.out.println("Low : " + low);
             }
         }
-        return nums[2*low];
+        return nums[2 * low];
     }
 
     public static void main(String[] args) {
         LonelyInteger lonelyInteger = new LonelyInteger();
         System.out.println(lonelyInteger.findLonely(new int[]{10, 9, 1, 2, 3, 3, 11, 2, 9, 1, 7, 7, 10}));
-        System.out.println(lonelyInteger.singleNonDuplicate(new int[]{1,1,2,2,3,4,4,5,5}));
+        System.out.println(lonelyInteger.singleNonDuplicate(new int[]{1, 1, 2, 2, 3, 4, 4, 5, 5}));
     }
 }

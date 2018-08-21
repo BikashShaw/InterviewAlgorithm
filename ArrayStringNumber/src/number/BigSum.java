849a3eec51a5f8135ba45981fc832bc84de71b9f
bikash-at-640172929504 <pZREc9L8W9MxlXf3PI5Dyct/MN+RdQ4lKjymK9f6i+g=>
2018-08-20 23:11:40 -0400

    reformatting

--- ArrayStringNumber/src/number/BigSum.java
+++ ArrayStringNumber/src/number/BigSum.java
@@ -11,13 +11,10 @@ import java.util.stream.Collectors;
 public class BigSum {
 
     public static String sum(int nums[]) {
-        List<String> collect = Arrays.stream(nums).mapToObj(Integer::toString).collect(Collectors.toList());
-        collect.sort((a, b) -> b.length() - a.length());
+        List<String> collect = Arrays.stream(nums).mapToObj(Integer::toString).sorted((a, b) -> b.length() - a.length()).collect(Collectors.toList());
         int longestNumber = collect.get(0).length();
 
-        collect = collect.stream().map(a -> {
-            return insertLeadingZeros(a, longestNumber);
-        }).collect(Collectors.toList());
+        collect = collect.stream().map(a -> insertLeadingZeros(a, longestNumber)).collect(Collectors.toList());
 
         collect.forEach(System.out::println);
 
@@ -26,17 +23,17 @@ public class BigSum {
         for (int i = 0; i < longestNumber; i++) {
             Integer stepSum = 0;
             for (String s : collect) {
-                char c = s.charAt(longestNumber - i - 1);;
-                Integer integer = Integer.valueOf(c+"");
-                stepSum+=integer;
+                char c = s.charAt(longestNumber - i - 1);
+                Integer integer = Integer.valueOf(c + "");
+                stepSum += integer;
             }
-            stepSum+=carry;
-            carry = stepSum /10;
-            stepSum =  stepSum % 10;
-            sum.insert(0,stepSum);
+            stepSum += carry;
+            carry = stepSum / 10;
+            stepSum = stepSum % 10;
+            sum.insert(0, stepSum);
         }
-        if(carry > 0){
-            sum.insert(0,carry);
+        if (carry > 0) {
+            sum.insert(0, carry);
         }
         return sum.toString();
     }
@@ -45,14 +42,14 @@ public class BigSum {
         StringBuilder stringBuilder = new StringBuilder(a);
         int initialLen = a.length();
         for (int i = 0; i < longestNumber - initialLen; i++) {
-            stringBuilder.insert(0,"0");
+            stringBuilder.insert(0, "0");
         }
 
         return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
-        int nums[] = {1001458909,1004570889,1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249};
+        int nums[] = {1001458909, 1004570889, 1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249, 5};
         System.out.println("Sum of " + Arrays.toString(nums) + " is " + sum(nums));
         nums = new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE};
         System.out.println("Sum of " + Arrays.toString(nums) + " is " + sum(nums));

b23c0345d7f9f380a20ab83bd0e14cb291dd95d1
uc0010 <BSHAW-EXTERNAL@scor.com>
2018-08-28 16:17:46 -0400

    OddEvenLinkedList

--- DataStructure/src/datastructure/linear/OddEvenLinkedList.java
+++ DataStructure/src/datastructure/linear/OddEvenLinkedList.java
@@ -5,7 +5,7 @@ package datastructure.linear;
  */
 public class OddEvenLinkedList {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -16,6 +16,31 @@ public class OddEvenLinkedList {
 
     //TODO
     public ListNode oddEvenList(ListNode head) {
-        return null;
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            ListNode next = current.next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        //[1,2,3,4,5]
+        ListNode head = new ListNode(1);
+        add(head, 2,3,4,5);
+
+        System.out.println(head);
+
+    }
+
+    private static void add(ListNode head, int ... values) {
+        ListNode node = head;
+
+        for (int value : values) {
+            node.next = new ListNode(value);
+            node = node.next;
+        }
     }
 }

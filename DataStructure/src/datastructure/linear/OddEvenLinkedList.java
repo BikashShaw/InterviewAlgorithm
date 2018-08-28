191426ba84246b08b0e1bbac51af7c07892b084e
uc0010 <BSHAW-EXTERNAL@scor.com>
2018-08-28 15:51:15 -0400

    Leetcode Odd Even Linked List

---
+++ DataStructure/src/datastructure/linear/OddEvenLinkedList.java
@@ -0,0 +1,21 @@
+package datastructure.linear;
+
+/**
+ * Description: https://leetcode.com/problems/odd-even-linked-list/description/
+ */
+public class OddEvenLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    //TODO
+    public ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+}

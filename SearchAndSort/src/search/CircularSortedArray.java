64e55a5037fc55356ef6ae58d47aea448b70b4ff
bikash-at-640172929504 <pZREc9L8W9MxlXf3PI5Dyct/MN+RdQ4lKjymK9f6i+g=>
2018-10-25 21:11:00 -0400

    Optimized, bug fix

--- SearchAndSort/src/search/CircularSortedArray.java
+++ SearchAndSort/src/search/CircularSortedArray.java
@@ -3,44 +3,48 @@ package search;
 import java.util.Arrays;
 
 /**
+ * Leetcode: https://leetcode.com/problems/search-in-rotated-sorted-array/
  * Created by Bikash on 3/28/2017.
  */
 public class CircularSortedArray {
 
-    public int search(int a[], int low, int high, int target) {
+    public int search(int nums[], final int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
         while (low <= high) {
-            int mid = (high + low) / 2;
-            if(target == a[mid]) { //Found target
+            mid = (high + low) / 2;
+            if (target == nums[mid]) { //Found target
                 return mid;
             }
-            if(a[mid] <= a[high]) { //right half is sorted
-                if(target > a[mid] && target < a[high]) {
+            if (nums[mid] <= nums[high]) { //right half is sorted
+                if (target > nums[mid] && target <= nums[high]) {
                     low = mid + 1; // search right sorted half
                 } else {
                     high = mid - 1; // search left sorted half
                 }
             } else { // left half is sorted
-                if(a[low] <= target && target<a[mid]) { //left half is sorted
-                    high = mid -1; // search left sorted half
+                if (nums[low] <= target && target <= nums[mid]) { //left half is sorted
+                    high = mid - 1; // search left sorted half
                 } else {
                     low = mid + 1; // search right sorted half
                 }
             }
 
         }
-        return  -1;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int a[] = {12,14,18,21,3,8,9};
+        int a[] = {12, 14, 18, 21, 3, 8, 9};
         CircularSortedArray circularSortedArray = new CircularSortedArray();
 
         System.out.println("Array: " + Arrays.toString(a));
 
-        System.out.println("Found 26: " + circularSortedArray.search(a, 0, a.length-1, 26));
-        System.out.println("Found 3: " + circularSortedArray.search(a, 0, a.length-1, 3));
-        System.out.println("Found 21: " + circularSortedArray.search(a, 0, a.length-1, 21));
-        System.out.println("Found 8: " + circularSortedArray.search(a, 0, a.length-1, 8));
-        System.out.println("Found 12: " + circularSortedArray.search(a, 0, a.length-1, 12));
+        System.out.println("Found 26: " + circularSortedArray.search(a, 26));
+        System.out.println("Found 3: " + circularSortedArray.search(a, 3));
+        System.out.println("Found 21: " + circularSortedArray.search(a, 21));
+        System.out.println("Found 8: " + circularSortedArray.search(a, 8));
+        System.out.println("Found 12: " + circularSortedArray.search(a, 12));
     }
 }
